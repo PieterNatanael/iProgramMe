453dae817d33a38f712b6d9809fa832a871976f7
PieterNatanael <pieternatanael@icloud.com>
2023-09-27 14:25:20 +0800

    Initial Commit

---
+++ iProgramMe/iProgramMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iProgramMeApp.swift
+//  iProgramMe
+//
+//  Created by Pieter Yoshua Natanael on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct iProgramMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
